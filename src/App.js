42cf35e32dda77fb5e05a9398cb96fccc354f8e5
Sahas <sahasvivek001@gmail.com>
2022-06-13 18:59:42 +0530

    feat: login & signup done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import NoteState from './context/Notes/NoteState';
+import About from './components/About';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Signup from './components/Signup';
+import Navbar from './components/Navbar';
+import Login from './components/Login';
+import Home from './components/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <NoteState>
+      <Router>
+        <Navbar />
+        <Routes>
+          {/* <Route path="/" element={<Home />} /> */}
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
+    </NoteState>
   );
 }
 
